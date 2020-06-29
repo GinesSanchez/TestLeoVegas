d377f4484b529b3625219e20bedf545db4a338ee
ginessan <ginesinho@gmail.com>
2020-06-29 18:06:29 +0200

    Add Firebase Crashlytics and Analytics pods to the project. Create project workspace.

---
+++ TestLeoVegas/Pods/Target Support Files/Pods-TestLeoVegas/Pods-TestLeoVegas-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestLeoVegas : NSObject
+@end
+@implementation PodsDummy_Pods_TestLeoVegas
+@end
---
+++ TestLeoVegas/Pods/Target Support Files/Pods-TestLeoVegasTests/Pods-TestLeoVegasTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestLeoVegasTests : NSObject
+@end
+@implementation PodsDummy_Pods_TestLeoVegasTests
+@end
